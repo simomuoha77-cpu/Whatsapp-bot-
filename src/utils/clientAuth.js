5194a9b9fe91939ad61389f713239ea5a1ce6bfa
Bitcoin <simomuoha603927@bitfreeze.nynmstp.mongodb.net>
2026-07-01 12:10:37 +0300

    Harden client session to always match its bot account

--- src/utils/clientAuth.js
+++ src/utils/clientAuth.js
@@ -1,11 +1,17 @@
-/**
- * Express middleware: blocks access to client dashboard routes unless
- * logged in as a client account (separate session namespace from the
- * platform admin login).
- */
+const { getClientAccountByBotId } = require('../db/clientAccounts');
+
 function requireClientAuth(req, res, next) {
-  if (req.session && req.session.clientBotId) return next();
-  return res.redirect('/client/login');
+  if (!req.session || !req.session.clientBotId) return res.redirect('/client/login');
+
+  getClientAccountByBotId(req.session.clientBotId)
+    .then((account) => {
+      if (!account) {
+        return req.session.destroy(() => res.redirect('/client/login'));
+      }
+      req.session.clientPhoneNumber = account.phone_number;
+      next();
+    })
+    .catch(() => res.redirect('/client/login'));
 }
 
 module.exports = { requireClientAuth };
